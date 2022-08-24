636145d6d301b28142c63553f7f8ea478fca5016
Szymon Malinowski <mszymon962@gmail.com>
2022-08-24 11:38:37 +0200

    Removing unnecessary imports

--- app/database.py
+++ app/database.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from starlette.config import Config
-from starlette.datastructures import Secret
-from databases import DatabaseURL
-from config import DATABASE_URL, database_url
+from config import database_url
 
 SQLALCHEMY_DATABASE_URL = database_url
 
